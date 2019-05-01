47b39ea04e1aa8f14407c903fd2b566447575a0c
Travis CI User <travis@example.org>
2019-05-01 06:44:06 +0000

    Documentation for cessen/ropey@dcd4428

---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["ropey"] = {"name":"","dirs":[{"name":"tree","dirs":[],"files":["mod.rs","node.rs","node_children.rs","node_text.rs","text_info.rs"]}],"files":["crlf.rs","iter.rs","lib.rs","rope.rs","rope_builder.rs","slice.rs","str_utils.rs"]};
+sourcesIndex["smallvec"] = {"name":"","dirs":[],"files":["lib.rs"]};
+createSourceSidebar();
